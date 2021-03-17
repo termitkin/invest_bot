2aacc3188131de68e9fe1b481b633038ead636b7
termitkin <bigos911@gmail.com>
2021-03-17 18:02:33 +0300

    jest removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,9 @@ module.exports = {
   env: {
     node: true,
     commonjs: true,
-    'jest/globals': true,
   },
   extends: ['eslint:recommended', 'airbnb'],
-  plugins: ['jest'],
+  plugins: [],
   parserOptions: {
     ecmaVersion: 12,
   },
@@ -13,18 +12,6 @@ module.exports = {
     'no-console': 'off',
     'operator-linebreak': [2, 'after'],
     'max-len': ['error', { code: 120 }],
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
+
   },
-  overrides: [
-    {
-      files: ['**/*.test.js'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
