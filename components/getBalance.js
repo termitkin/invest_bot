ba0fbf545797897e78a3e7090155f9c85348cab1
termitkin <bigos911@gmail.com>
2021-04-04 22:30:23 +0300

    EUR support added in getBalance

--- components/getBalance.js
+++ components/getBalance.js
@@ -1,18 +1,23 @@
 const api = require('../utils/api');
-const { FIGI_USDRUB } = require('../utils/CONSTANTS');
+const { FIGI_USDRUB, FIGI_EURRUB } = require('../utils/CONSTANTS');
 
 const getBalance = async () => {
   const usd = await api.orderbookGet({ figi: FIGI_USDRUB, depth: 1 });
+  const eur = await api.orderbookGet({ figi: FIGI_EURRUB, depth: 1 });
+
   const { positions } = await api.portfolio();
   let sum = 0;
 
   positions.forEach((item) => {
     if (item.expectedYield.currency === 'RUB') {
       sum += item.expectedYield.value;
-    } else {
+    } else if (item.expectedYield.currency === 'USD') {
       sum += item.expectedYield.value * usd.lastPrice;
+    } else if (item.expectedYield.currency === 'EUR') {
+      sum += item.expectedYield.value * eur.lastPrice;
     }
   });
+
   return sum.toFixed(2);
 };
 
