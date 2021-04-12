0e82bdf9797f96556ea59d4982bf80db64067e63
termitkin <bigos911@gmail.com>
2021-04-12 10:16:23 +0300

    variable renamed in currentDate.js

--- utils/currentDate.js
+++ utils/currentDate.js
@@ -3,11 +3,11 @@ const currentDate = () => {
   const hours = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
   const minutes = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
   const seconds = d.getSeconds() < 10 ? `0${d.getSeconds()}` : d.getSeconds();
-  const date = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
+  const day = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
   const month = d.getMonth() < 10 ? `0${d.getMonth()}` : d.getMonth();
   const year = d.getFullYear();
 
-  return `${date}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
 };
 
 module.exports = currentDate;
